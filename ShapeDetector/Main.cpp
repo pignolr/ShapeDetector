aa2ac281d94d36610c7d01b32cc13b145a157bb2
Rémi Pignol <remi.pignol@epitech.eu>
2019-11-06 20:39:16 +0900

    Add load Image

--- ShapeDetector/Main.cpp
+++ ShapeDetector/Main.cpp
@@ -1,14 +1,55 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
 #include <iostream>
+#include <list>
+#include <exception>
+#include <sstream>
 
-int main()
+std::list<cv::Mat> loadImageFromArguments(int ac, char **av)
 {
-	cv::Mat image = cv::Mat::zeros(300, 600, CV_8UC3);
-	cv::circle(image, cv::Point(250, 150), 100, cv::Scalar(0, 255, 128), -100);
-	cv::circle(image, cv::Point(350, 150), 100, cv::Scalar(255, 255, 255), -100);
-	cv::imshow("Display Window", image);
-	cv::waitKey(0);
-	return 0;
+	std::stringstream error;
+	if (ac < 2) {
+		error << "Usage: av[0] img1 [...]" << std::endl;
+		throw std::invalid_argument(error.str());
+	}
+	std::list<cv::Mat> images;
+	for (int i = 1; i < ac; ++i) {
+		auto image = cv::imread(av[i], cv::IMREAD_COLOR);
+		if (!image.data) {
+			error << "Error: Could not open or find the image: " << av[i] << std::endl;
+		}
+		else {
+			images.push_back(image);
+		}
+	}
+	if (error.str().length())
+		throw std::invalid_argument(error.str());
+	return images;
+}
+
+void detectShapeImages(std::list<cv::Mat> images)
+{
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+	for (auto& image : images) {
+		cv::imshow("Display window", image);
+
+		cv::waitKey(0);
+	}
+}
+
+int main(int ac, char **av)
+{
+	try
+	{
+		auto images = loadImageFromArguments(ac, av);
+		detectShapeImages(images);
+	}
+	catch (std::invalid_argument & e)
+	{
+		std::cout << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
